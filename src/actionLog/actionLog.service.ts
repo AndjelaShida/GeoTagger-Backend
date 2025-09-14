6558743226acc2824d721355465fa4487759c643
Andjela Šida <andjelasheeda@gmail.com>
2025-09-14 11:47:40 +0200

    getLast100Log function i  actionlog service

--- src/actionLog/actionLog.service.ts
+++ src/actionLog/actionLog.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ActionLog } from 'generated/prisma';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -6,10 +7,26 @@ export class ActionLogService {
   constructor(private prisma: PrismaService) {}
 
   //ADMIN MOZE DA VIDI POSLEDNJIH 100 LOGOVA-admin only
-async getLast100Log()
+  async getLast100Log(currentUserId: string): Promise<ActionLog[]> {
+    //pronadji korisnika sa njegovim ulogama
+    const user = await this.prisma.user.findUnique({
+      where: { id: currentUserId },
+      include: { roles: true },
+    });
 
+    if (!user) throw new ForbiddenException('User not found.');
 
-
+    //proveri da li korisnik ima admin ulogu
+    const isAdmin = user.roles.some((role) => role.name === 'ADMIN');
+    if (!isAdmin) {
+      throw new ForbiddenException('You are not allowed to do this action.');
+    }
+    //vrati poslednjih 100 logova
+    return await this.prisma.actionLog.findMany({
+      take: 100,
+      orderBy: { createdAt: 'desc' },
+    });
+  }
 
   //LOGOVI PO KORINSIKU
   //FILTRIRANJE PO TIPU AKCIJE(click, scroll, input, change)
